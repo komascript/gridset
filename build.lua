39a5ed4dfc8a46d0e56bad5926ff82ed1c1bc29b
Markus Kohm <komascript@noreply.github.com>
2020-02-12 11:03:29 +0100

    missing line break in md files added

--- build.lua
+++ build.lua
@@ -41,8 +41,8 @@ function update_tag (file,content,tagname,tagdate)
 			  "[" .. tagdate .. " v" .. tagname )
    elseif string.match (file, "%.md$") then
       return string.gsub (content,
-			  "\nRelease: %d%d%d%d%-%d%d%-%d%d v%d+%.%d+\n",
-			  "\nRelease: " .. tagdate .. " v" .. tagname .. "\n")
+			  "\nRelease: %d%d%d%d%-%d%d%-%d%d v%d+%.%d+  \n",
+			  "\nRelease: " .. tagdate .. " v" .. tagname .. "  \n")
    elseif string.match (file, "%.lua$") then
       return string.gsub (content,
 			  '\nrelease_info = "%d%d%d%d%-%d%d-%d%d v%d+%.%d+"\n"',
